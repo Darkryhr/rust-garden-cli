81d0daa8857ec9e383668b599bc813b5c45f24ee
gabriel <gabyk800@gmail.com>
2021-10-31 13:25:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Digital Garden
+
+A CLI tool for the creation and maintenance of Digital Gardens
+
+## Commands
+
+### Setting the garden path
+
+```shell
+GARDEN_PATH=~/github/my-digital-garden garden write
+garden -p ~/github/my-digital-garden write
+garden --garden_path ~/github/my-digital-garden write
+```
+
+## write
+
+```shell
+garden write
+garden write -t "Some Title"
+```
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::path::PathBuf;
+
+use color_eyre::eyre::{eyre, Result, WrapErr};
+use digital_garden::write;
+use directories::UserDirs;
+use structopt::StructOpt;
+
+/// A CLI for growing and curating a digital garden
+///
+#[derive(StructOpt, Debug)]
+#[structopt(name = "garden")]
+struct Opt {
+    #[structopt(parse(from_os_str), short = "p", long, env)]
+    garden_path: Option<PathBuf>,
+
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    /// write something in your garden
+    ///
+    /// This command opens the $EDITOR, waits for you
+    /// to write something, then saves the file
+    Write {
+        /// Optionally set a title
+        #[structopt(short, long)]
+        title: Option<String>,
+    },
+}
+
+fn get_default_garden_dir() -> Result<PathBuf> {
+    let user_dirs = UserDirs::new().ok_or_else(|| eyre!("Could not find home directory"))?;
+    Ok(user_dirs.home_dir().join(".garden"))
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let opt = Opt::from_args();
+    let garden_path = match opt.garden_path {
+        Some(pathbuf) => Ok(pathbuf),
+        None => get_default_garden_dir().wrap_err("`garden_path` was not supplied"),
+    }?;
+    match opt.cmd {
+        Command::Write { title } => write(garden_path, title),
+    }
+}
